6a70ac5ab8ee628a4a13d17207f88f383f0baf09
DmitryAnatolievich <bezrukdmitry@gmail.con>
2020-09-11 17:00:06 +0300

    Initial Commit

---
+++ CinemaApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CinemaApp
+//
+//  Created by Dmitry Anatolievich on 11.09.2020.
+//  Copyright © 2020 Dmitry Anatolievich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
